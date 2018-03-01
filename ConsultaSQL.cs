f1f0dea7b59482f5a135c86b7c33ef339bd61768
Kabir Alberto <35808948+StayTrue10@users.noreply.github.com>
2018-03-01 15:22:02 -0500

    1.0
    falta que imprima

---
+++ ConsultaSQL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Busqueda
+{
+    class ConsultaSQL
+    {
+        private SqlConnection conexion = new SqlConnection("Data Source = 10.10.50.201; Initial Catalog = RUTACRITICA; user id=sa; password=Pr0c3s0.12");
+        private DataSet ds;
+
+        public DataTable MostrarDatos()
+        {
+            conexion.Open();
+            SqlCommand cmd = new SqlCommand("select * from Salidas", conexion);
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            ds = new DataSet();
+            ad.Fill(ds, "tabla");
+            conexion.Close();
+            return ds.Tables["tabla"];
+        }
+
+        public DataTable Buscar(string ticket)
+        {
+            conexion.Open();
+            SqlCommand cmd = new SqlCommand(string.Format("select * from Salidas where Ticket like '%{0}%'", ticket), conexion);
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            ds = new DataSet();
+            ad.Fill(ds, "tabla");
+            conexion.Close();
+            return ds.Tables["tabla"];
+        }
+
+
+
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Busqueda
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ConsultaSQL sql = new ConsultaSQL();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = sql.MostrarDatos();
+        }
+
+        private void tbFolio_TextChanged(object sender, EventArgs e)
+        {
+            if (tbFolio.Text != "") dataGridView1.
+                    DataSource = sql.Buscar(tbFolio.Text);
+            else
+                dataGridView1.DataSource = sql.MostrarDatos();
+        }
+
+        
+        
+    }
+}
