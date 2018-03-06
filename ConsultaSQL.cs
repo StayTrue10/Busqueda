7e56bbecec896ca0ee100a3ab0e5ba2a0e946d9e
Kabir Alberto <35808948+StayTrue10@users.noreply.github.com>
2018-03-06 18:16:07 -0500

    2.0
    conexion drf1, *no busca*

--- ConsultaSQL.cs
+++ ConsultaSQL.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,33 +10,27 @@ namespace Busqueda
 {
     class ConsultaSQL
     {
-        private SqlConnection conexion = new SqlConnection("Data Source = 10.10.50.201; Initial Catalog = RUTACRITICA; user id=sa; password=Pr0c3s0.12");
+        private SqlConnection conexion = new SqlConnection("Data Source = 10.10.50.201; Initial Catalog = ANCONA_PROD; user id=sa; password=Pr0c3s0.12");
         private DataSet ds;
-
         public DataTable MostrarDatos()
         {
             conexion.Open();
-            SqlCommand cmd = new SqlCommand("select * from Salidas", conexion);
+            SqlCommand cmd = new SqlCommand("select ItemCode, Dscription, Quantity from DRF1 where DocEntry = '100'", conexion);
             SqlDataAdapter ad = new SqlDataAdapter(cmd);
             ds = new DataSet();
             ad.Fill(ds, "tabla");
             conexion.Close();
             return ds.Tables["tabla"];
         }
-
         public DataTable Buscar(string ticket)
         {
             conexion.Open();
-            SqlCommand cmd = new SqlCommand(string.Format("select * from Salidas where Ticket like '%{0}%'", ticket), conexion);
+            SqlCommand cmd = new SqlCommand(string.Format("select ItemCode, Dscription, Quantity from DRF1 where DocEntry = '100'", ticket), conexion);
             SqlDataAdapter ad = new SqlDataAdapter(cmd);
             ds = new DataSet();
             ad.Fill(ds, "tabla");
             conexion.Close();
             return ds.Tables["tabla"];
         }
-
-
-
-
     }
 }
